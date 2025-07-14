de7ae201353873c1232f20f950571501589b7672
Celestial-Entity <zarathustra44@tuta.io>
2025-07-14 21:08:53 +0530

    multifile(project.py)

--- main.py
+++ main.py
@@ -1,57 +1,6 @@
-import os
-import shutil
+from scripts import head
+from scripts import parser
+from scripts import render
+from scripts import base
 
-I = "element/chronicle.kv"
-O = "to-world"
-
-os.makedirs(O, exist_ok=True) # creates the output DIR.
-shutil.copy("style.css", O)   # copies the style.css to output DIR.
-
-def parse_kv():
-
-    glossary = []
-    BLOK = {}
-    last_key = None
-
-    with open(I, "r") as file:
-        for line in file:
-            line = line.strip()
-            if not line:
-                if BLOK:
-                    glossary.append(BLOK)
-                    BLOK = {}
-                    last_key = None
-                continue
-            if ' : ' in line:
-                key, value = line.split(" : ", 1)
-                BLOK[key] = value
-                last_key = key
-            else:
-                if last_key:
-                    BLOK[last_key] = f"{BLOK[last_key]}\n{line}"
-        if BLOK:
-            glossary.append(BLOK)
-
-    return glossary
-        
-data = parse_kv()
-
-def write_data(glossary):
-    for BLOK in glossary:
-        title = BLOK.get("TITL", "Untitled")
-        description = BLOK.get("BODY", "No content available.")
-
-        with open("layout/base.html", "r") as file:
-            template_base = file.read()
-        
-        html_base = template_base.format(title = title, description = description) 
-
-        url = title.replace(' ', '-').lower() + ".html"
-        with open(os.path.join(O, url), "w", encoding='utf-8') as h:
-            h.write(html_base)
-        print(url)
-    
-def main():
-    write_data(data)
-    
-main()
+# surprisingly just importing the base, project runs them when I run main.py. 
